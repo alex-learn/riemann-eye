c21bf3cb5adecd88c436a767fb91457c0ec917ef
gkos <danielhilst@gmail.com>
2013-02-07 22:24:58 -0200

    routes added

--- controllers/default.rb
+++ controllers/default.rb
@@ -5,4 +5,29 @@ class Eye < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/grid/:query' do |qstr|
+    haml :grid, :locals => { :qstr => qstr }
+  end
+
+  get '/problems' do
+    haml :grid, :locals => { :qstr => '(state != "ok")' }
+  end
+
+  get '/host/:host' do |host|
+    haml :grid, :locals => { :qstr => "(host = \"#{host}\")" } 
+  end
+
+  get '/service/:service' do |service|
+    haml :grid, :locals => { :qstr => "(service = \"#{service}\")" }
+  end
+
+  get '/state/:state' do |state|
+    haml :grid, :locals => { :qstr => "(state = \"#{state}\")" }
+  end
+
+  get '/notstate/:state' do |state|
+    haml :grid, :locals => { :qstr => "(state != \"#{state}\")" }
+  end
+
 end
--- eye.rb
+++ eye.rb
@@ -4,4 +4,15 @@ require 'riemann/client'
 class Eye < Sinatra::Base
   require_relative 'models/default'
   require_relative 'controllers/default'
+
+
+  def self.client
+    @@client ||= Riemann::Client.new(host: "localhost",
+                                     port: 5555);
+  end
+
+  def query(*a)
+    self.class.client.query(*a).events || []
+  end
+
 end
